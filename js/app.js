c7a44b2b18c51c14127c572edfab1e1ee448cc01
bernkaztel <bernkaztel@hotmail.com>
2018-04-09 15:31:43 -0500

    quitando alert de app.js

--- js/app.js
+++ js/app.js
@@ -6,6 +6,6 @@ import Menu from "./Menu.js"
 function CreateMenu () {
     return   <div><Menu /></div>;
 }
-alert(':)');
+
 
 ReactDOM.render(<CreateMenu/>, document.getElementById('react-container'))
